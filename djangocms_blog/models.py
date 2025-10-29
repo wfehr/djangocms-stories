4f610af6208b306a504125b0cc8b24ccbf3f86ff
Wolfgang Fehr <24782511+wfehr@users.noreply.github.com>
2025-10-29 07:44:07 +0100

    fix: use correct tags for meta.tag

--- djangocms_blog/models.py
+++ djangocms_blog/models.py
@@ -477,13 +477,6 @@ class Post(KnockerModel, models.Model):
                 return get_thumbnailer(self.main_image).get_thumbnail(thumbnail_options).height
             return self.main_image.height
 
-    def get_tags(self):
-        """
-        Returns the list of object tags as comma separated list
-        """
-        taglist = [tag.name for tag in self.tags.all()]
-        return ",".join(taglist)
-
     def get_author(self):
         """
         Return the author (user) objects
@@ -696,7 +689,7 @@ class PostContent(BlogMetaMixin, ModelMeta, models.Model):
         """
         Returns the list of object tags as comma separated list
         """
-        taglist = [tag.name for tag in self.tags.all()]
+        taglist = [tag.name for tag in self.post.tags.all()]
         return ",".join(taglist)
 
     def __str__(self):
